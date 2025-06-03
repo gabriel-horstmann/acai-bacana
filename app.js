066a5710b594b8cfb6a28ec7442e3017b7617ee0
AS-Gaspar <airtontunni@gmail.com>
2025-06-03 17:03:36 -0300

    trocando de maquina

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const path = require('path')
-const 
 
 const app = express()
 const PORT = process.env.PORT || 3000
@@ -9,6 +8,9 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.listen(PORT, () => [
+const pedidosRoutes = require('./src/routes/pedidos')
+app.use('/api/pedidos', pedidosRoutes)
+
+app.listen(PORT, () => {
     console.log(`Server rodando em http://localhost:${PORT}`)
-])
+})
