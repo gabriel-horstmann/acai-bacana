6d66b35a40a87a03f6b376d1ae545e80e33dd3a2
AS-Gaspar <airtontunni@gmail.com>
2025-06-07 11:30:52 -0300

    create migration file to remove unused column data

---
+++ migrations/20250607140843_remover_coluna_data.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.table('pedidos', function(table) {
+        table.dropColumn('data')
+    })  
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema.table('pedidos', function(table) {
+        table.date('data').notNullable()
+    }) 
+};
