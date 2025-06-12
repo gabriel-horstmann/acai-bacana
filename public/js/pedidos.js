c2e70164bffcdb1b1e4a997df08151f2f4e24a59
AS-Gaspar <airtontunni@gmail.com>
2025-06-12 15:25:57 -0300

    teste, primeiro fetch a api realizado

--- public/js/pedidos.js
+++ public/js/pedidos.js
@@ -1,1 +1,20 @@
-/* Carregar pedidos */
+async function loadData() {
+    const pedidosTable = document.getElementById('pedidosTable')
+    const API = await fetch("http://localhost:3000/api/pedidos")
+    const objects = await API.json()  
+    objects.data.forEach(object => {
+        let trPedido = document.createElement("tr")
+        let tdCliente = document.createElement("td")
+        let tdStatus = document.createElement("td")
+        let tdValor = document.createElement("td")
+        tdCliente.innerHTML = object.cliente
+        tdStatus.innerHTML = object.status
+        tdValor.innerHTML = object.valor_pedido
+        trPedido.appendChild(tdCliente)
+        trPedido.appendChild(tdStatus)
+        trPedido.appendChild(tdValor)
+        pedidosTable.appendChild(trPedido)
+    })
+}
+
+
