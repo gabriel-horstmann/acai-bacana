024ed8d70a05c0ff88105caf40434bf10d479359
AS-Gaspar <airtontunni@gmail.com>
2025-06-15 11:42:59 -0300

    acessando id do produto no html

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
-require('dotenv').config()
-const express = require('express')
-const path = require('path')
-const cors = require('cors')
+require("dotenv").config()
+const express = require("express")
+const path = require("path")
+const cors = require("cors")
 const app = express()
 const PORT = process.env.PORT || 3000
 
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, "public")))
 
-const pedidosRoutes = require('./src/routes/pedidos')
-app.use('/api/pedidos', pedidosRoutes)
+const pedidosRoutes = require("./src/routes/pedidos")
+app.use("/api/pedidos", pedidosRoutes)
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, "public/views/index.html"))
+})
+
+app.get('/pedidos.html', (req, res) => {
+    res.sendFile(path.join(__dirname, "public/views/pedidos.html"))
+})
 
 app.listen(PORT, () => {
-    console.log(`Server rodando em http://localhost:${PORT}`)
+  console.log(`Server rodando em http://localhost:${PORT}`)
 })
--- public/js/pedidos.js
+++ public/js/pedidos.js
@@ -7,12 +7,17 @@ async function loadData() {
         let tdCliente = document.createElement("td")
         let tdStatus = document.createElement("td")
         let tdValor = document.createElement("td")
+        let linkId = document.createElement("a") 
         tdCliente.innerHTML = object.cliente
         tdStatus.innerHTML = object.status
         tdValor.innerHTML = object.valor_pedido
+        linkId.href = object.id_pedido
+        linkId.textContent = 'Acessar pedido'
+        linkId.target = '_blank'
         trPedido.appendChild(tdCliente)
         trPedido.appendChild(tdStatus)
         trPedido.appendChild(tdValor)
+        trPedido.appendChild(linkId)
         pedidosTable.appendChild(trPedido)
     })
 }
